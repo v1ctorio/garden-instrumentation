37d55a99bf70cf7279f635f43e173288033a3047
Vic <vicxd@pm.me>
2026-01-25 23:00:09 +0100

    fix: semicolon :p

--- database/schema.sql
+++ database/schema.sql
@@ -1,27 +1,27 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     slack_id TEXT PRIMARY KEY,
     join_date TIMESTAMPTZ NOT NULL,
     timezone TEXT,
     join_origin TEXT
-)
+);
 
-CREATE TABLE events (
+CREATE TABLE IF NOT EXISTS  events (
     id BIGSERIAL PRIMARY KEY,
     event_time TIMESTAMPTZ NOT NULL,
     slack_id TEXT NOT NULL,
     event_name TEXT NOT NULL,
     program TEXT,
     metadata JSONB NOT NULL DEFAULT '{}'
-)
+);
 
 
-CREATE TABLE allowed_events (
+CREATE TABLE IF NOT EXISTS allowed_events (
     event_name TEXT PRIMARY KEY
-)
+);
 
 INSERT INTO allowed_events VALUES 
     ('wthc_message_sent'),
-    ('introduction_sent')
+    ('introduction_sent');
 
 
 ALTER TABLE events
@@ -29,14 +29,14 @@ ADD CONSTRAINT fk_events_event_name
 FOREIGN KEY (event_name)
 REFERENCES allowed_events (event_name);
 
-CREATE INDEX idx_events_user_time
+CREATE INDEX IF NOT EXISTS idx_events_user_time
   ON events (slack_id, event_time);
 
-CREATE INDEX idx_events_event_name
+CREATE INDEX IF NOT EXISTS idx_events_event_name
   ON events (event_name);
 
-CREATE INDEX idx_events_time
+CREATE INDEX IF NOT EXISTS idx_events_time
   ON events (event_time);
 
-CREATE INDEX idx_events_metadata
+CREATE INDEX IF NOT EXISTS idx_events_metadata
   ON events USING GIN (metadata);
